25f3659fb7c34348fef2c25f1a8adbbb7e6de06d
rushabh782 <rushabhpatel877@gmail.com>
2024-01-31 23:48:46 +0530

    added sigin page and signin with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,12 +1,16 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const config =   {
-    apiKey: "AIzaSyABAMtr083esTJBK2vOvRSYkdXQt9MNVLs",
-    authDomain: "chat-web-app-dfcaa.firebaseapp.com",
-    projectId: "chat-web-app-dfcaa",
-    storageBucket: "chat-web-app-dfcaa.appspot.com",
-    messagingSenderId: "96531847703",
-    appId: "1:96531847703:web:d95bebe3b4aef25a941a52"
-  };
+const config = {
+  apiKey: 'AIzaSyABAMtr083esTJBK2vOvRSYkdXQt9MNVLs',
+  authDomain: 'chat-web-app-dfcaa.firebaseapp.com',
+  projectId: 'chat-web-app-dfcaa',
+  storageBucket: 'chat-web-app-dfcaa.appspot.com',
+  messagingSenderId: '96531847703',
+  appId: '1:96531847703:web:d95bebe3b4aef25a941a52',
+};
 
-  const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
