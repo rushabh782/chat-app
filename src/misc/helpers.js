aebc701a48ceff15aef61646c775a1e187342566
rushabh782 <rushabhpatel877@gmail.com>
2024-02-29 00:48:27 +0530

    grouped messages by data

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -49,3 +49,19 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
 
   return updates;
 }
+
+// groupBy(messages,(msgItem)=> msgItem.createdAt)
+
+export function groupBy(array, groupingKeyFn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyFn(item);
+
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+
+    result[groupingKey].push(item);
+
+    return result;
+  }, {});
+}
